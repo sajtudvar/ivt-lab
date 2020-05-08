58884609007d35d2b783e16d5b45f6a08adfd51a
sajtudvar <djindic@gmail.com>
2020-05-08 10:45:21 +0200

    awsome comment

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -29,7 +29,7 @@ public class TorpedoStore {
       }
     }
   }
-
+  // awsome comment is placed which increased code readability by 73.6%
   public boolean fire(int numberOfTorpedos){
     if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
       throw new IllegalArgumentException("numberOfTorpedos");
